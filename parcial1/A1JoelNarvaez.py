5a1fa6ea8e3d18cd8ddce5fa15493f67e59ad42f
JoelNarvaez <joelnarmar20@gmail.com>
2026-08-12 18:04:20 -0600

    Se agregan comentarios al codigo de la actividad 1

--- parcial1/A1JoelNarvaez.py
+++ parcial1/A1JoelNarvaez.py
@@ -5,24 +5,30 @@
 #             los conceptos fundamentales de python
 #             como introduccion al lenguaje
 
+#Operadores aritmeticos
 5 + 6
 10 % 3
 5 ** 3
 9 // 2
 
+#Variables
 nombre = 5
 type(nombre)
 nombre = "Juan"
 type(nombre)
 nombre = 5.2
 
+#Cadenas de texto e impresion
 mensaje = """Este es un mensaje 
 ... con tres saltos
 ... de linea"""
 print(mensaje)
 
+
 numero1 = 5
 numero2  = 7
+
+#Condicional if con su respectivo else
 if numero1>numero2:
     print("El numero 1 es mayor ")
 else:
