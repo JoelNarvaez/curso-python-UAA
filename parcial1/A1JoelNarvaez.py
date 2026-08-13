28b56a2f1b622df947cf32f0b22c2fa910a93c8a
JoelNarvaez <joelnarmar20@gmail.com>
2026-08-12 18:00:22 -0600

    Actividad 1: sintaxis basica de python

---
+++ parcial1/A1JoelNarvaez.py
@@ -0,0 +1,29 @@
+# Autor: Joel Narvaez Martinez
+# Materia: Programacion en python
+# Fecha: 8/12/2026
+# Actividad: La actividad consite en practicar
+#             los conceptos fundamentales de python
+#             como introduccion al lenguaje
+
+5 + 6
+10 % 3
+5 ** 3
+9 // 2
+
+nombre = 5
+type(nombre)
+nombre = "Juan"
+type(nombre)
+nombre = 5.2
+
+mensaje = """Este es un mensaje 
+... con tres saltos
+... de linea"""
+print(mensaje)
+
+numero1 = 5
+numero2  = 7
+if numero1>numero2:
+    print("El numero 1 es mayor ")
+else:
+    print("El numero 2 es amyor")
